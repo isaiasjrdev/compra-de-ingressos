35f7091c6c767adee7d3f2b92b1688133d91fd51
Isaias Junior <isaiscastro56@gmail.com>
2024-07-03 20:21:54 -0300

    calculando a compra para pista

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 function comprar() {
    let tipo = document.querySelector('#tipo-ingresso');
-   let qtd = document.querySelector('#qtd');
-   
+   let qtd = parseInt(document.querySelector('#qtd').value);
+
+   if (tipo.value == 'pista') {
+      comprarPista(qtd);
+   } else if (tipo.value == 'superior') {
+      comprarSuperior(qtd);
+   } else {
+      comprarInferior(qtd);
+   }
+}
+
+function comprarPista(qtd) {
+   let qtdPista = parseInt(document.querySelector('#qtd-pista').textContent);
+   if (qtd > qtdPista) {
+      alert('Quantidade indisponível para tipo pista');
+   } else {
+      qtdPista = qtdPista - qtd;
+      document.querySelector('#qtd-pista').textContent = qtdPista;
+      alert('Compra realizada com sucesso!');
+   }
 }
